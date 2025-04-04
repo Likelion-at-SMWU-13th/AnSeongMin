0d6098f1b4c1937999ff28e566344a47a04a07f1
minnnnnnni <dkstodtjdals@gmail.com>
2025-04-05 00:56:45 +0900

    주석 추가

--- week04/accounts/views.py
+++ week04/accounts/views.py
@@ -32,3 +32,5 @@ def logout(request):
     if request.user.is_authenticated:
         auth_logout(request)
         return redirect("login")
+    # 여기에 else문으로 로그아웃되어 있는 경우 로그아웃 버튼을 눌렀을 때에 대한 기능을 처리해주지 않았기 때문에
+    # 로그아웃된 상태에서 로그아웃 버튼 누르면 에러가 뜨는게 정상이다. 
