00e99df1ca12bb54fb4bf235b6518a97fb43f01b
minnnnnnni <dkstodtjdals@gmail.com>
2025-04-06 00:47:56 +0900

    회원가입 기능 추가

--- week04_hw/accounts/views.py
+++ week04_hw/accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignupForm
 
-# Create your views here.
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid(): # 폼이 유효하면 회원가입 성공
+            form.save()     # 정보 저장하고(?)
+            return redirect("login") # login 페이지로 이동
+    else:
+        form = SignupForm()
+
+    return render(request, "signup.html", {"form":form})
