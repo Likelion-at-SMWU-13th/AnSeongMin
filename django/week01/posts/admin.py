3aa978c99d6cd176a85ee0ca357c9e4ee05c243e
minnnnnnni <dkstodtjdals@gmail.com>
2025-04-13 21:32:43 +0900

    Post 모델 수정 및 migrate 다시 하기

--- django/week01/posts/admin.py
+++ django/week01/posts/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from .models import Post
-from .models import Comments
 
 # Register your models here.
 admin.site.register(Post)
-admin.site.register(Comments)
--- django/week01/posts/models.py
+++ django/week01/posts/models.py
@@ -8,11 +8,5 @@ class Post(models.Model):
     created_at = models.DateTimeField(verbose_name='작성일')
     view_count = models.IntegerField(verbose_name='조회수')
 
-# 댓글 Comments model
-class Comments(models.Model):
-    content = models.TextField(verbose_name='내용')
-    created_at = models.DateTimeField(verbose_name='작성일')
-    like_count = models.IntegerField(verbose_name='좋아요수')
-
 
     
