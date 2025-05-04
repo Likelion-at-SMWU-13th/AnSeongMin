1fc89d18f440fcdd4fab7460c8f00c0aa572487e
minnnnnnni <dkstodtjdals@gmail.com>
2025-05-04 12:27:40 +0900

    introduce에 fbv 연결

--- django/week02_hw/introduce/views.py
+++ django/week02_hw/introduce/views.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse
 
 
 def introduce_view(request):
-    print(f'request.method:{request.method}') # 로그에 찍는코드
-    if request.method == 'GET':
-        print(f'request.GET: {request.GET}')
-    elif request.method == 'POST':
-        print(f'request.POST: {request.POST}')
-    return render(request, 'introduce.html')
+    data = {
+        'name':'안성민',
+        'generation':'13기',
+        'part':'백엔드',
+        'favorite_food':'마라샹궈',
+    }
+    return render(request, 'introduce.html', data)
