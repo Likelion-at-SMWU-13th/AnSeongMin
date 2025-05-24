a5fa1f09da32e2f1f304eaad5790004b3ac50237
minnnnnnni <dkstodtjdals@gmail.com>
2025-05-25 03:14:07 +0900

    add: 회원가입 뷰 파일 생성/ feat: User 모델용 시리얼라이저 코드 작성

--- django/week04_hw/posts/serializers.py
+++ django/week04_hw/posts/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Post
+from django.contrib.auth.models import User 
 
 class PostModelSerializer(ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__' # 모든 필드를 직렬화
-        
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
