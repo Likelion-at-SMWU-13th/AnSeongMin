3bc6e841558dec64bbc2c508327b95a43be768e7
minnnnnnni <dkstodtjdals@gmail.com>
2025-06-29 23:03:40 +0900

    Feat: serializers.py 수정, views.py 구현

--- django/5th_hw/blog/serializers.py
+++ django/5th_hw/blog/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Post, Comment
 
-class PostModelSerializer(ModelSerializer):
-    class Meta:
-        model = Post # Post 모델을 직렬화
-        fields = '__all__' # 모든 필드를 직렬화 (어떤 필드를 포함하여 직렬화할지)
-
 class CommentModelSerializer(ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+
+class PostModelSerializer(ModelSerializer): # 글 전체보기 (상세보기)
+    comments = CommentModelSerializer(many=True, read_only=True, source='comment_set')
+
+    class Meta:
+        model = Post # Post 모델을 직렬화
+        fields = '__all__' # 모든 필드를 직렬화 (어떤 필드를 포함하여 직렬화할지)
+
+class PostListSerializer(ModelSerializer): # 글 목록보기 
+    class Meta:
+        model = Post
+        fields = ['id', 'content', 'created_at'] # 최소한 댓글은 안보이도록 함.
