d7aef029f661dc050042b5f78a242668d7d9faac
minnnnnnni <dkstodtjdals@gmail.com>
2025-07-20 23:02:34 +0900

    Feat: id 말고 studentID 로 단건조회, 수정, 삭제 하도록 수정

--- spring/week01/seminar/src/main/java/com/likelion/seminar/student/controller/StudentController.java
+++ spring/week01/seminar/src/main/java/com/likelion/seminar/student/controller/StudentController.java
@@ -25,32 +25,47 @@ public class StudentController {
         return this.studentDTOList;
     }
 
-    @GetMapping("{id}")
-    public StudentDTO readStudentById(@PathVariable("id") int id) {
-        System.out.println("포스트 단일 조회");
-        return this.studentDTOList.get(id);
+    @GetMapping("{studentID}")
+    public Object readStudent(@PathVariable("studentID") Long studentID) {
+        System.out.println("학생 단일 조회");
+        for (StudentDTO studentDto : this.studentDTOList) {
+            if (studentDto.getStudentID().equals(studentID)) {
+                return studentDto;
+            }
+        }
+        return "해당 studentID를 가진 학생이 존재하지 않습니다.";
     }
 
-    @PutMapping("{id}")
-    public void updateStudent(
-            @PathVariable("id") int id,
+    @PutMapping("{studentID}")
+    public Object updateStudent(
+            @PathVariable("studentID") Long studentID,
             @RequestBody StudentDTO studentDto) {
-        StudentDTO targetStudent = this.studentDTOList.get(id);
-        if(studentDto.getStudentID()!=null){
-            targetStudent.setStudentID(studentDto.getStudentID());
+        for (StudentDTO targetStudent : this.studentDTOList) {
+            if (targetStudent.getStudentID().equals(studentID)) {
+                if (studentDto.getName() != null) {
+                    targetStudent.setName(studentDto.getName());
+                }
+                if (studentDto.getDateOfBirth() != null) {
+                    targetStudent.setDateOfBirth(studentDto.getDateOfBirth());
+                }
+                if (studentDto.getStudentID() != null) {
+                    targetStudent.setStudentID(studentDto.getStudentID());
+                }
+                return "학생 정보 수정 완료";
+            }
         }
-        if(studentDto.getName()!=null){
-            targetStudent.setName(studentDto.getName());
-        }
-        if(studentDto.getDateOfBirth()!=null){
-            targetStudent.setDateOfBirth(studentDto.getDateOfBirth());
-        }
-        this.studentDTOList.set(id, targetStudent);
+        return "해당 studentID를 가진 학생이 존재하지 않습니다.";
     }
 
-    @DeleteMapping("{id}")
-    public void deleteStudent(@PathVariable("id") int id) {
-        this.studentDTOList.remove(id);
+    @DeleteMapping("{studentID}")
+    public Object deleteStudent(@PathVariable("studentID") Long studentID) {
+        for (StudentDTO student : this.studentDTOList) {
+            if (student.getStudentID().equals(studentID)) {
+                this.studentDTOList.remove(student);
+                return "학생이 삭제되었습니다.";
+            }
+        }
+        return "해당 studentID를 가진 학생이 존재하지 않습니다.";
     }
 
 
