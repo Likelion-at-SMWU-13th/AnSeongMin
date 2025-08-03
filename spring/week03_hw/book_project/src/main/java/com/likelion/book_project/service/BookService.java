c6470b5e049cc8bd1377ef0de4d846a1c5e2e1e1
minnnnnnni <dkstodtjdals@gmail.com>
2025-08-03 20:43:30 +0900

    Feat:books.html 수정, java 메인함수 추가

--- spring/week03_hw/book_project/src/main/java/com/likelion/book_project/service/BookService.java
+++ spring/week03_hw/book_project/src/main/java/com/likelion/book_project/service/BookService.java
@@ -1,4 +1,21 @@
 package com.likelion.book_project.service;
 
+import com.likelion.book_project.model.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@Service
 public class BookService {
+
+    private List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public List<Book> findAll() {
+        return books;
+    }
 }
