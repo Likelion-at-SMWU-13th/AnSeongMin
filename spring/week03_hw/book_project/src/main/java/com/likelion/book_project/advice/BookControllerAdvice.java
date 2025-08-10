cb5d35d29b1ea2968df38a2164752e4f4b8e6058
minnnnnnni <dkstodtjdals@gmail.com>
2025-08-10 23:59:28 +0900

    feat

--- spring/week03_hw/book_project/src/main/java/com/likelion/book_project/advice/BookControllerAdvice.java
+++ spring/week03_hw/book_project/src/main/java/com/likelion/book_project/advice/BookControllerAdvice.java
@@ -10,14 +10,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice // 전역 예외 처리 담당
 public class BookControllerAdvice {
 
-    @ExceptionHandler(BookNotFoundException.class) // BookNotFoundException 발생 시 처리
+    @ExceptionHandler(BookNotFoundException.class)
     public ResponseEntity<ErrorDetails> handleBookNotFound() {
         ErrorDetails errorDetails = new ErrorDetails();
         errorDetails.setMessage("책을 찾을 수 없습니다.");
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDetails);
     }
 
-    @ExceptionHandler(DuplicateBookException.class) // DuplicateBookException 발생 시 처리
+    @ExceptionHandler(DuplicateBookException.class)
     public ResponseEntity<ErrorDetails> handleDuplicateBook() {
         ErrorDetails errorDetails = new ErrorDetails();
         errorDetails.setMessage("이미 존재하는 책입니다.");
