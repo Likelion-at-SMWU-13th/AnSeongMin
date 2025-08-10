9165e60b70638acd227813ec03c44c4e161c1b9e
minnnnnnni <dkstodtjdals@gmail.com>
2025-08-10 23:20:34 +0900

    feat/swagger conf 추가, bookController에 swagger 어노테이션 추가

--- spring/week03_hw/book_project/src/main/java/com/likelion/book_project/controller/BookController.java
+++ spring/week03_hw/book_project/src/main/java/com/likelion/book_project/controller/BookController.java
@@ -2,12 +2,19 @@ package com.likelion.book_project.controller;
 
 import com.likelion.book_project.model.Book;
 import com.likelion.book_project.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Tag(name = "Book API", description = "도서 목록 웹앱 API")
 @Controller
 public class BookController {
 
@@ -17,6 +24,11 @@ public class BookController {
         this.bookService = bookService;
     }
 
+
+    @Operation(summary = "GET BOOK API", description = "등록된 모든 도서 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공")
+    })
     @GetMapping("/books")
     public String viewBooks(Model model) {
         var books = bookService.findAll();
@@ -25,6 +37,17 @@ public class BookController {
         return "books.html";
     }
 
+
+    @Operation(summary = "POST BOOK API", description = "새로운 도서 등록")
+    @Parameters({
+            @Parameter(name = "title", description = "도서 제목", required = true),
+            @Parameter(name = "author", description = "저자 이름", required = true),
+            @Parameter(name = "price", description = "도서 가격", required = true)
+    })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "등록 성공"),
+            @ApiResponse(responseCode = "409", description = "중복된 책이 이미 존재함")
+    })
     @PostMapping("/books")
     public String addBook(
             @RequestParam String title,
