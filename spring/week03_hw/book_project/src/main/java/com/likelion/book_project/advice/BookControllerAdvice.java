dbfabdf2c3fdf0067d83ade6e451438106b59401
minnnnnnni <dkstodtjdals@gmail.com>
2025-08-10 23:05:46 +0900

    feat/service 로직 예외처리 구현

--- spring/week03_hw/book_project/src/main/java/com/likelion/book_project/advice/BookControllerAdvice.java
+++ spring/week03_hw/book_project/src/main/java/com/likelion/book_project/advice/BookControllerAdvice.java
@@ -1,4 +1,26 @@
 package com.likelion.book_project.advice;
 
+import com.likelion.book_project.exception.BookNotFoundException;
+import com.likelion.book_project.exception.DuplicateBookException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class BookControllerAdvice {
+
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<String> handleBookNotFound(BookNotFoundException e) {
+        return new ResponseEntity<>("존재하지 않는 책입니다.", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DuplicateBookException.class)
+    public ResponseEntity<String> handleDuplicateBook(DuplicateBookException e) {
+        return new ResponseEntity<>("이미 존재하는 책입니다.", HttpStatus.CONFLICT);
+    }
+
+
+
+
 }
--- spring/week03_hw/book_project/src/main/java/com/likelion/book_project/service/BookService.java
+++ spring/week03_hw/book_project/src/main/java/com/likelion/book_project/service/BookService.java
@@ -1,5 +1,7 @@
 package com.likelion.book_project.service;
 
+import com.likelion.book_project.exception.BookNotFoundException;
+import com.likelion.book_project.exception.DuplicateBookException;
 import com.likelion.book_project.model.Book;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +13,26 @@ public class BookService {
 
     private List<Book> books = new ArrayList<>();
 
+    public List<Book> findAll() {
+        return books;
+    }
+
     public void addBook(Book book) {
+        for (Book b : books) {
+            if (b.getTitle().equals(book.getTitle())) {
+                throw new DuplicateBookException(book.getTitle()+"은 이미 존재하는 책입니다.");
+            }
+        }
         books.add(book);
     }
 
-    public List<Book> findAll() {
-        return books;
+
+    public Book findBookByTitle(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equals(title)) {
+                return book;
+            }
+        }
+        throw new BookNotFoundException("책을 찾을 수 없습니다.");
     }
 }
