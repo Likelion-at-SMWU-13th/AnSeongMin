69e517bf0e10724665602923755239d6c433b738
minnnnnnni <dkstodtjdals@gmail.com>
2025-08-10 23:10:21 +0900

    feat/book컨트롤러 어드바이스 수정, errordetails 구현

--- spring/week03_hw/book_project/src/main/java/com/likelion/book_project/advice/BookControllerAdvice.java
+++ spring/week03_hw/book_project/src/main/java/com/likelion/book_project/advice/BookControllerAdvice.java
@@ -7,20 +7,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice // 전역 예외 처리 담당
 public class BookControllerAdvice {
 
-    @ExceptionHandler(BookNotFoundException.class)
-    public ResponseEntity<String> handleBookNotFound(BookNotFoundException e) {
-        return new ResponseEntity<>("존재하지 않는 책입니다.", HttpStatus.NOT_FOUND);
+    @ExceptionHandler(BookNotFoundException.class) // BookNotFoundException 발생 시 처리
+    public ResponseEntity<ErrorDetails> handleBookNotFound() {
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setMessage("책을 찾을 수 없습니다.");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDetails);
     }
 
-    @ExceptionHandler(DuplicateBookException.class)
-    public ResponseEntity<String> handleDuplicateBook(DuplicateBookException e) {
-        return new ResponseEntity<>("이미 존재하는 책입니다.", HttpStatus.CONFLICT);
+    @ExceptionHandler(DuplicateBookException.class) // DuplicateBookException 발생 시 처리
+    public ResponseEntity<ErrorDetails> handleDuplicateBook() {
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setMessage("이미 존재하는 책입니다.");
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorDetails);
     }
-
-
-
-
 }
